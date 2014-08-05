eb588409f2e970faca6399c70385ca9cef3a30a5
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2014-08-05 11:18:52 +0200

    Copying ko-archetype to become base for crud-archetype

---
+++ crud-archetype/src/main/resources/archetype-resources/src/main/java/Main.java
@@ -0,0 +1,56 @@
+package ${package};
+
+import net.java.html.boot.BrowserBuilder;
+#if ($nbrwsr == "true")
+import org.netbeans.api.nbrwsr.OpenHTMLRegistration;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+#end
+
+public final class Main {
+    private Main() {
+    }
+    
+    public static void main(String... args) throws Exception {
+        BrowserBuilder.newBrowser().
+            loadPage("pages/index.html").
+            loadClass(Main.class).
+            invoke("onPageLoad", args).
+            showAndWait();
+        System.exit(0);
+    }
+#if ($nbrwsr == "true")
+    //
+    // the following annotations generate registration for NetBeans,
+    // they are harmless in other packaging schemes
+    //
+    
+    @ActionID(
+        category = "Games",
+        id = "${package}.OpenPage"
+    )
+    @OpenHTMLRegistration(
+        url="index.html",
+        displayName = "Open Your Page",
+        iconBase = "${package.replace('.','/')}/icon.png"
+    )
+    @ActionReferences({
+        @ActionReference(path = "Menu/Window"),
+        @ActionReference(path = "Toolbars/Games")
+    })
+    //
+    // end of NetBeans actions registration
+    //
+#end
+
+    /**
+     * Called when the page is ready.
+     */
+    public static void onPageLoad() throws Exception {
+        Data d = new Data();
+        d.setMessage("Hello World from HTML and Java!");
+        d.applyBindings();
+    }
+    
+}
